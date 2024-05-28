2daf282d6ccfb690c4e4a1faedca3d895774fde6
MicheleCalliari <michele.calliari04@gmail.com>
2024-05-28 10:33:24 +0200

    finito Farmacia con jd

--- StreamThis/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.streamthis {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.streamthis to javafx.fxml;
-    exports Main;
-    opens Main to javafx.fxml;
-}
